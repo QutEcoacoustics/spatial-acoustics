dd64f1bb4262975bf46df02d0003b57398db444e
Marina D. A. Scarpelli <ninascarpelli@gmail.com>
2019-11-19 17:09:47 +1000

    Create useful-commands.R
    Script with useful commands and functions

---
+++ useful-commands.R
@@ -0,0 +1,5 @@
+#Useful commands
+
+getDataPath <- function (...) {
+  return(file.path("C:/Users/n10393021/OneDrive - Queensland University of Technology/Documents/PhD/Project/Fieldwork_Bowra",  ...))
+}
