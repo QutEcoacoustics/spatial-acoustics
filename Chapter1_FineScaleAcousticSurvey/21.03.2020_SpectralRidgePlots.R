5d96fa7a85816472615359f1ff133424251b92f6
Marina D. A. Scarpelli <ninascarpelli@gmail.com>
2020-03-24 12:24:06 +1000

    Update 21.03.2020_SpectralRidgePlots.R

--- Chapter1_FineScaleAcousticSurvey/21.03.2020_SpectralRidgePlots.R
+++ Chapter1_FineScaleAcousticSurvey/21.03.2020_SpectralRidgePlots.R
@@ -1,15 +1,28 @@
 library(tidyverse)
 library(ggridges)
 library(ggplot2)
+library(plot.matrix)
+
+#getDataPath <- function (...) {
+  #return(file.path("C:/Users/n10393021/OneDrive - Queensland University of Technology/Documents/PhD/Project",  ...))
+#}
 
 getDataPath <- function (...) {
-  return(file.path("C:/Users/n10393021/OneDrive - Queensland University of Technology/Documents/PhD/Project",  ...))
+  return(file.path("C:/Users/Nina Scarpelli/OneDrive - Queensland University of Technology/Documents/PhD/Project",  ...))
 }
 
-ACI_Spectral <- read.csv(getDataPath("Fieldwork_Bowra", "Oct2019", "ResultsIndices_Channel1", "JZ001_WB28" ,"BOW-JZ1-WB28_20191014_180000.wav", "Towsey.Acoustic", "BOW-JZ1-WB28_20191014_180000__Towsey.Acoustic.ACI.csv"))
+for (file in files) {
+  
+}
+
+ACI_Spectral <- read.csv(getDataPath("Fieldwork_Bowra", "Oct2019", "ResultsIndices_Channel1", "JZ001_WB28" ,"BOW-JZ1-WB28_20191014_180000.wav", "Towsey.Acoustic", "BOW-JZ1-WB28_20191014_180000__Towsey.Acoustic.ACI.csv"), row.names = 2) %>% 
+  select(2:258)
+
+object.size(ACI_Spectral)
 
 t <- t(ACI_Spectral)
 
-d <- ggplot(ACI_Spectral, aes(x = 2, y = 3:259))
+
+d <- plot(t, border = NA)
 
 g <- d + geom_bar()
