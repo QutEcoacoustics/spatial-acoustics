1d491b3b787bb4deb6619e0c423e36ef883e2d95
Marina Scarpelli Drummond De Almeida <n10393021@qut.edu.au>
2020-07-01 16:37:34 +1000

    Update 30.06.2020_RSNDVISAVI_PreProcessing.R

--- Chapter1_FineScaleAcousticSurvey/30.06.2020_RSNDVISAVI_PreProcessing.R
+++ Chapter1_FineScaleAcousticSurvey/30.06.2020_RSNDVISAVI_PreProcessing.R
@@ -3,20 +3,19 @@ library(purrr)
 
 getDataPath <- function (...) {
   return(file.path("C:/Users/scarp/OneDrive - Queensland University of Technology/Documents/PhD/Project/Chapter1_FineScaleAcousticSurvey",  ...))
+  
 }
 
-files <- list.files(path = getDataPath("RemoteSensing", "2_SentinelHub_Landsat_SAVIExtracted"), pattern = ".csv", recursive = T, full.names = T)
-name <- basename(files)
-name <- gsub(pattern = ".csv", replacement = "", x = name)
 
-df <- list()
+getDataPath <- function (...) {
+  return(file.path("C:/Users/n10393021/OneDrive - Queensland University of Technology/Documents/PhD/Project/Chapter1_FineScaleAcousticSurvey",  ...))
+  
+}
 
-for (file in files) {
-  read.csv(file) %>% 
-  select(., "SAVI_AVG", "ID", "DATE") %>% 
-  assign(paste("df", basename(files), sep = ""), file)
-} 
+files <- list.files(path = getDataPath("RemoteSensing", "2_SentinelHub_Landsat_NDVIExtracted"), pattern = ".csv", recursive = T, full.names = T)
+
+df <- lapply(files, read.csv) %>% 
+  map_df(., select, c("NDVI_AVERAGE", "ID", "DATE")) %>% 
+  write.csv(., getDataPath("RemoteSensing", "2_SentinelHub_Landsat_NDVIExtracted", "01.07.2020_AVGNDVI.csv"))
 
 
-%>% 
-  do.call(rbind, files) -> df
